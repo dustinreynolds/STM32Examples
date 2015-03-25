c905724e68fff766e9272b5da4a0f4da09e93228
Dustin Reynolds <dustinreynolds@users.noreply.github.com>
2015-03-24 19:46:21 -0500

    Updating USART example to have the recommended structInit call.

--- USART2/src/uart.c
+++ USART2/src/uart.c
@@ -57,12 +57,12 @@ void uart_Configuration(uart_init_mode_t mode)
 		uart_NVIC_USART2_deinit();
 	}
 
+	USART_StructInit(&USART_InitStructure);
 	USART_InitStructure.USART_BaudRate = 9600;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;
 	USART_InitStructure.USART_Parity = USART_Parity_No;
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
 	USART_Init(USART2, &USART_InitStructure);

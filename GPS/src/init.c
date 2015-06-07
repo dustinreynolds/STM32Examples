df00467132fa82a74e9fb6ae46480982cf34c1b1
Dustin Reynolds <dustinreynolds@users.noreply.github.com>
2015-06-07 17:38:47 -0500

    Adding GPS example that only copies from one UART (UART5), to USART2, at 38400 Baud.

---
+++ GPS/src/init.c
@@ -0,0 +1,83 @@
+/*
+ * init.c
+ *
+ *  Created on: Mar 21, 2015
+ *      Author: Dustin
+ */
+
+#include "stm32l1xx.h"
+
+
+void init_RCC_Configuration(void)
+{
+	/* --------------------------- System Clocks Configuration -----------------*/
+	/* USART2 clock enable */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	/* USART1 clock enable */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	/* UART5 clock enable */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART5, ENABLE);
+
+	/* GPIOA clock enable */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+
+	/* GPIOB clock enable */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+
+	/* GPIOD clock enable */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOD, ENABLE);
+}
+
+void init_GPIO_Configuration(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/*-------------------------- GPIO Configuration ----------------------------*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3; // PA.2 USART2_TX, PA.3 USART1_RX
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Connect USART pins to AF */
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+
+	/*-------------------------- GPIO Configuration ----------------------------*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Connect USART pins to AF */
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
+
+	/*-------------------------- GPIO Configuration ----------------------------*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	/* Connect USART pins to AF */
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource2, GPIO_AF_UART5);
+
+	/*-------------------------- GPIO Configuration ----------------------------*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIOB->BSRRL |= GPIO_Pin_6;  //disable gps
+}
+
+
---
+++ GPS/src/main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 21, 2015
+ *      Author: Dustin
+ */
+#include "stm32l1xx.h"
+#include "uart.h"
+
+
+int main(void)
+{
+	USART_TypeDef * USARTx = USART2;
+	init_RCC_Configuration();
+
+	init_GPIO_Configuration();
+
+	uart_Configuration(USARTx, UART_INTERRUPT_RX);
+	uart_OutString(USARTx,"WELcome to Nucleo L152RE\r\n");
+
+	uart_Configuration(UART5, UART_INTERRUPT_RX);
+
+	uart_OutString(USARTx,"Welcome to Nucleo L152RE\r\n");
+
+	while(1){
+		//Let GPS send us stuff in the background
+	}
+}

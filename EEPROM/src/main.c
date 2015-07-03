079295a1b6974512a4aace039944faf747826c52
dustin reynolds <dreynolds@sparxeng.com>
2015-07-02 20:41:30 -0500

    Packet writing to EEPROM and reading back from eeprom is working as expected for single packet sequences.

--- EEPROM/src/main.c
+++ EEPROM/src/main.c
@@ -7,6 +7,7 @@
 #include "stm32l1xx.h"
 #include "uart.h"
 #include "packet_eeprom.h"
+#include "string.h"
 
 typedef enum {FAILED = 0, PASSED = !FAILED} TestStatus;
 
@@ -26,6 +27,8 @@ uint32_t NbrOfPage = 0, j = 0, Address = 0;
 int main(void)
 {
 	USART_TypeDef * USARTx = USART2;
+	parser_t eeprom_parser;
+
 	init_RCC_Configuration();
 
 	init_GPIO_Configuration();
@@ -33,114 +36,113 @@ int main(void)
 	uart_Configuration(USARTx, UART_POLLING);
 	uart_OutString(USARTx,"Welcome to Nucleo L152RE\r\n");
 
-	//unlock eeprom
-	DATA_EEPROM_Unlock();
+//	//unlock eeprom
+//	//TODO clean this up
+//	//DATA_EEPROM_Unlock();
+//
+//	/* Clear all pending flags */
+//	FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
+//			| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR | FLASH_FLAG_OPTVERRUSR);
+//
+//	/*  Data EEPROM Fast Word program of FAST_DATA_32 at addresses defined by
+//	      DATA_EEPROM_START_ADDR and DATA_EEPROM_END_ADDR */
+//	Address = DATA_EEPROM_START_ADDR;
+//
+//	NbrOfPage = ((DATA_EEPROM_END_ADDR - Address) + 1 ) >> 2;
+//
+//	/* Erase the Data EEPROM Memory pages by Word (32-bit) */
+//	for(j = 0; j < NbrOfPage; j++)
+//	{
+//		FLASHStatus = DATA_EEPROM_EraseWord(Address + (4 * j));
+//	}
+//
+//	/* Check the correctness of written data */
+//	while(Address < DATA_EEPROM_END_ADDR)
+//	{
+//		if(*(__IO uint32_t*)Address != 0x0)
+//		{
+//			DataMemoryProgramStatus = FAILED;
+//		}
+//		Address = Address + 4;
+//	}
+//
+//	Address = DATA_EEPROM_START_ADDR;
+//
+//	/* Program the Data EEPROM Memory pages by Word (32-bit) */
+//	while(Address <= DATA_EEPROM_END_ADDR )
+//	{
+//		FLASHStatus = DATA_EEPROM_FastProgramWord(Address, FAST_DATA_32);
+//
+//		if(FLASHStatus == FLASH_COMPLETE)
+//		{
+//			Address = Address + 4;
+//		}
+//		else
+//		{
+//			FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
+//					| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR);
+//		}
+//	}
+//
+//	Address = DATA_EEPROM_START_ADDR;
+//
+//	/* Check the correctness of written data */
+//	while(Address < DATA_EEPROM_END_ADDR)
+//	{
+//		if(*(__IO uint32_t*)Address != FAST_DATA_32)
+//		{
+//			DataMemoryProgramStatus = FAILED;
+//		}
+//		Address = Address + 4;
+//	}
+//
+//	/*  Data EEPROM Word program of DATA_32 at addresses defined by DATA_EEPROM_START_ADDR
+//	      and DATA_EEPROM_END_ADDR */
+//	Address = DATA_EEPROM_START_ADDR;
+//
+//	while(Address <= DATA_EEPROM_END_ADDR )
+//	{
+//		FLASHStatus = DATA_EEPROM_ProgramWord(Address, DATA_32);
+//
+//		if(FLASHStatus == FLASH_COMPLETE)
+//		{
+//			Address = Address + 4;
+//		}
+//		else
+//		{
+//			FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
+//					| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR);
+//		}
+//	}
+//
+//	DATA_EEPROM_Lock();
+//
+//	Address = DATA_EEPROM_START_ADDR;
+//
+//	/* Check the correctness of written data */
+//	while(Address < DATA_EEPROM_END_ADDR)
+//	{
+//		if(*(__IO uint32_t*)Address != DATA_32)
+//		{
+//			DataMemoryProgramStatus = FAILED;
+//		}
+//		Address = Address + 4;
+//	}
+//
+//	if (DataMemoryProgramStatus != FAILED){
+//		uart_OutString(USARTx,"Successfully programmed eeprom");
+//	}else{
+//		uart_OutString(USARTx,"Failed programming eeprom");
+//	}
 
-	/* Clear all pending flags */
-	FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
-			| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR | FLASH_FLAG_OPTVERRUSR);
 
-	/*  Data EEPROM Fast Word program of FAST_DATA_32 at addresses defined by
-	      DATA_EEPROM_START_ADDR and DATA_EEPROM_END_ADDR */
 	Address = DATA_EEPROM_START_ADDR;
-
-	NbrOfPage = ((DATA_EEPROM_END_ADDR - Address) + 1 ) >> 2;
-
-	/* Erase the Data EEPROM Memory pages by Word (32-bit) */
-	for(j = 0; j < NbrOfPage; j++)
-	{
-		FLASHStatus = DATA_EEPROM_EraseWord(Address + (4 * j));
-	}
-
-	/* Check the correctness of written data */
-	while(Address < DATA_EEPROM_END_ADDR)
-	{
-		if(*(__IO uint32_t*)Address != 0x0)
-		{
-			DataMemoryProgramStatus = FAILED;
-		}
-		Address = Address + 4;
-	}
-
-	Address = DATA_EEPROM_START_ADDR;
-
-	/* Program the Data EEPROM Memory pages by Word (32-bit) */
-	while(Address <= DATA_EEPROM_END_ADDR )
-	{
-		FLASHStatus = DATA_EEPROM_FastProgramWord(Address, FAST_DATA_32);
-
-		if(FLASHStatus == FLASH_COMPLETE)
-		{
-			Address = Address + 4;
-		}
-		else
-		{
-			FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
-					| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR);
-		}
-	}
-
-	Address = DATA_EEPROM_START_ADDR;
-
-	/* Check the correctness of written data */
-	while(Address < DATA_EEPROM_END_ADDR)
-	{
-		if(*(__IO uint32_t*)Address != FAST_DATA_32)
-		{
-			DataMemoryProgramStatus = FAILED;
-		}
-		Address = Address + 4;
-	}
-
-	/*  Data EEPROM Word program of DATA_32 at addresses defined by DATA_EEPROM_START_ADDR
-	      and DATA_EEPROM_END_ADDR */
-	Address = DATA_EEPROM_START_ADDR;
-
-	while(Address <= DATA_EEPROM_END_ADDR )
-	{
-		FLASHStatus = DATA_EEPROM_ProgramWord(Address, DATA_32);
-
-		if(FLASHStatus == FLASH_COMPLETE)
-		{
-			Address = Address + 4;
-		}
-		else
-		{
-			FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
-					| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR);
-		}
-	}
-
-	Address = DATA_EEPROM_START_ADDR;
-
-	/* Check the correctness of written data */
-	while(Address < DATA_EEPROM_END_ADDR)
-	{
-		if(*(__IO uint32_t*)Address != DATA_32)
-		{
-			DataMemoryProgramStatus = FAILED;
-		}
-		Address = Address + 4;
-	}
-
-	if (DataMemoryProgramStatus != FAILED)
-	{
-		/* OK */
-		/* Turn on LD1 */
-		//STM_EVAL_LEDOn(LED1);
-		uart_OutString(USARTx,"Successfully programmed eeprom");
-	}
-	else
-	{
-		/* KO */
-		/* Turn on LD2 */
-		//STM_EVAL_LEDOn(LED2);
-		uart_OutString(USARTx,"Failed programming eeprom");
-	}
+	packet_parser_init(&eeprom_parser);
 
 	while(1){
 
 		uint16_t Data;
+		packet_eeprom_t packet_result;
 
 		/*Only get a character if it is ready*/
 		if(USART_GetFlagStatus(USARTx, USART_FLAG_RXNE) != RESET){
@@ -150,7 +152,29 @@ int main(void)
 			while(USART_GetFlagStatus(USARTx, USART_FLAG_TXE) == RESET); // Wait for Empty
 
 			//USART_SendData(USARTx, Data); // Echo Char
-			packet_eeprom_parser(USARTx, Data);
+			Address = DATA_EEPROM_START_ADDR;
+			packet_result = packet_eeprom_parser(Data, &eeprom_parser);
+
+			if (packet_result == PKT_SUCCESS){
+				uint8_t dataBuffer[100];
+				uint16_t pos = 0;
+
+				packet_eeprom_prepare_packet(&eeprom_parser,dataBuffer, &pos);
+				packet_eeprom_write(dataBuffer,pos, &Address);
+
+				Address = DATA_EEPROM_START_ADDR;
+				memset(dataBuffer, 0, sizeof(dataBuffer));
+				packet_parser_init(&eeprom_parser);
+
+				packet_eeprom_read_packet(USARTx, &Address, &eeprom_parser);
+				//uart_OutBuffer(USARTx, dataBuffer, pos);
+			}else if (packet_result == PKT_FAILURE){
+				char buffer[40];
+				sprintf(buffer,"CRC Fail, Calc %04x, Recv %04x\r\n",
+						eeprom_parser.crc,
+						(eeprom_parser.packet.crc[1] << 8) |eeprom_parser.packet.crc[0]);
+				uart_OutString(USARTx, buffer);
+			}
 		}
 	}
 }
--- EEPROM/src/packet_eeprom.c
+++ EEPROM/src/packet_eeprom.c
@@ -39,6 +39,39 @@
 #include "crc_16bit_algorithm_dnp.h"
 #include "uart.h"
 
+void _packet_find_crc_with_stx(USART_TypeDef * USARTx){
+	/*//CA D0 05 04 00 00 00 A5 C0 0A 3A
+	//CA D0 05 04 00 00 00 EB C0 00 93
+	//CA D0 05 04 00 00 01 31 C6 C0 0A
+	//CA D0 05 04 00 00 01 64 96 C0 00
+	//CA D0 05 04 00 00 01 B5 C0 0A 1B
+	//CA D0 05 04 00 00 01 B8 67 C0 0A 	//CA D0 05 04 00 00 01 ED 37 C0 00 	//CA D0 05 04 00 00 01 FB C0 00 B2 
+	//CA D0 05 04 00 00 02 85 C0 0A 78 
+	//CA D0 05 04 00 00 02 CB C0 00 D1 	//CA D0 05 04 00 00 03 04 52 C0 0A 	//CA D0 05 04 00 00 03 51 02 C0 00 	//CA D0 05 04 00 00 03 8D F3 C0 0A 	//CA D0 05 04 00 00 03 95 C0 0A 59 	//CA D0 05 04 00 00 03 D8 A3 C0 00 	//CA D0 05 04 00 00 03 DB C0 00 F0 	//CA D0 05 04 00 00 04 1C FC C0 00 	//CA D0 05 04 00 00 04 49 AC C0 0A 	//CA D0 05 04 00 00 04 95 5D C0 00 	//CA D0 05 04 00 00 04 AB C0 00 17 	//CA D0 05 04 00 00 04 C0 0A 43 C0 0A 	//CA D0 05 04 00 00 04 E5 C0 0A BE 	//CA D0 05 04 00 00 05 BB C0 00 36 	//CA D0 05 04 00 00 05 F5 C0 0A 9F 	//CA D0 05 04 00 00 06 29 68 C0 00 	//CA D0 05 04 00 00 06 7C 38 C0 0A 	//CA D0 05 04 00 00 06 8B C0 00 55 	//CA D0 05 04 00 00 06 A0 C9 C0 00 	//CA D0 05 04 00 00 06 C5 C0 0A FC 	//CA D0 05 04 00 00 06 F5 99 C0 0A 	//CA D0 05 04 00 00 07 9B C0 00 74 	//CA D0 05 04 00 00 07 D5 C0 0A DD 	//CA D0 05 04 00 00 08 24 C0 0A 32 	//CA D0 05 04 00 00 08 6A C0 00 9B 	//CA D0 05 04 00 00 09 28 77 C0 00 	//CA D0 05 04 00 00 09 34 C0 0A 13 	//CA D0 05 04 00 00 09 7A C0 00 BA 	//CA D0 05 04 00 00 09 7D 27 C0 0A 	//CA D0 05 04 00 00 09 A1 D6 C0 00 	//CA D0 05 04 00 00 09 F4 86 C0 0A 	//CA D0 05 04 00 00 0A 04 C0 0A 70 	//CA D0 05 04 00 00 0A 4A C0 00 D9 	//CA D0 05 04 00 00 0B 14 C0 0A 51 	//CA D0 05 04 00 00 0B 1D E3 C0 00 	//CA D0 05 04 00 00 0B 48 B3 C0 0A 	//CA D0 05 04 00 00 0B 5A C0 00 F8 	//CA D0 05 04 00 00 0B 94 42 C0 00 	//CA D0 05 04 00 00 0B C1 12 C0 0A 	//CA D0 05 04 00 00 0C 05 4D C0 0A 	//CA D0 05 04 00 00 0C 2A C0 00 1F 	//CA D0 05 04 00 00 0C 50 1D C0 00 	//CA D0 05 04 00 00 0C 64 C0 0A B6 	//CA D0 05 04 00 00 0C 8C EC C0 0A 	//CA D0 05 04 00 00 0C D9 BC C0 00 	//CA D0 05 04 00 00 0D 3A C0 00 3E 	//CA D0 05 04 00 00 0D 74 C0 0A 97 	//CA D0 05 04 00 00 0E 0A C0 00 5D 	//CA D0 05 04 00 00 0E 30 D9 C0 0A 	//CA D0 05 04 00 00 0E 44 C0 0A F4 	//CA D0 05 04 00 00 0E 65 89 C0 00 	//CA D0 05 04 00 00 0E B9 78 C0 0A 	//CA D0 05 04 00 00 0E EC 28 C0 00 	//CA D0 05 04 00 00 0F 1A C0 00 7C 	//CA D0 05 04 00 00 0F 54 C0 0A D5 	//CA D0 05 04 00 00 10 32 E7 C0 0A 	//CA D0 05 04 00 00 10 67 B7 C0 00 	//CA D0 05 04 00 00 10 B7 C0 0A 0B 	//CA D0 05 04 00 00 10 BB 46 C0 0A 	//CA D0 05 04 00 00 10 EE 16 C0 00 	//CA D0 05 04 00 00 10 F9 C0 00 A2 	//CA D0 05 04 00 00 11 A7 C0 0A 2A 	//CA D0 05 04 00 00 11 E9 C0 00 83 	//CA D0 05 04 00 00 12 07 73 C0 0A 	//CA D0 05 04 00 00 12 52 23 C0 00 	//CA D0 05 04 00 00 12 8E D2 C0 0A 	//CA D0 05 04 00 00 12 97 C0 0A 49 	//CA D0 05 04 00 00 12 C0 00 C0 0A 9A 	//CA D0 05 04 00 00 12 D9 C0 00 E0 	//CA D0 05 04 00 00 12 DB 82 C0 00 	//CA D0 05 04 00 00 13 87 C0 0A 68 	//CA D0 05 04 00 00 13 C9 C0 00 C1 	//CA D0 05 04 00 00 14 B9 C0 00 26 	//CA D0 05 04 00 00 14 F7 C0 0A 8F 	//CA D0 05 04 00 00 15 1F DD C0 00 	//CA D0 05 04 00 00 15 4A 8D C0 0A 	//CA D0 05 04 00 00 15 96 7C C0 00 	//CA D0 05 04 00 00 15 A9 C0 00 07 	//CA D0 05 04 00 00 15 C3 2C C0 0A 	//CA D0 05 04 00 00 15 E7 C0 0A AE 	//CA D0 05 04 00 00 16 99 C0 00 64 	//CA D0 05 04 00 00 16 D7 C0 0A CD 	//CA D0 05 04 00 00 17 2A 49 C0 00 	//CA D0 05 04 00 00 17 7F 19 C0 0A 	//CA D0 05 04 00 00 17 89 C0 00 45 	//CA D0 05 04 00 00 17 A3 E8 C0 00 	//CA D0 05 04 00 00 17 C7 C0 0A EC 	//CA D0 05 04 00 00 17 F6 B8 C0 0A 	//CA D0 05 04 00 00 18 2B 56 C0 00 	//CA D0 05 04 00 00 18 36 C0 0A 03 	//CA D0 05 04 00 00 18 78 C0 00 AA 	//CA D0 05 04 00 00 18 7E 06 C0 0A 	//CA D0 05 04 00 00 18 A2 F7 C0 00 	//CA D0 05 04 00 00 18 F7 A7 C0 0A 	//CA D0 05 04 00 00 19 26 C0 0A 22 	//CA D0 05 04 00 00 19 68 C0 00 8B 	//CA D0 05 04 00 00 1A 16 C0 0A 41 	//CA D0 05 04 00 00 1A 1E C2 C0 00 	//CA D0 05 04 00 00 1A 4B 92 C0 0A 	//CA D0 05 04 00 00 1A 58 C0 00 E8 	//CA D0 05 04 00 00 1A 97 63 C0 00 	//CA D0 05 04 00 00 1A C2 33 C0 0A 	//CA D0 05 04 00 00 1B 06 C0 0A 60 	//CA D0 05 04 00 00 1B 48 C0 00 C9 	//CA D0 05 04 00 00 1C 38 C0 00 2E 	//CA D0 05 04 00 00 1C 76 C0 0A 87 	//CA D0 05 04 00 00 1D 06 6C C0 0A 	//CA D0 05 04 00 00 1D 28 C0 00 0F 	//CA D0 05 04 00 00 1D 53 3C C0 00 	//CA D0 05 04 00 00 1D 66 C0 0A A6 	//CA D0 05 04 00 00 1D 8F CD C0 0A 	//CA D0 05 04 00 00 1D DA 9D C0 00 	//CA D0 05 04 00 00 1E 18 C0 00 6C 	//CA D0 05 04 00 00 1E 56 C0 0A C5 	//CA D0 05 04 00 00 1F 08 C0 00 4D 	//CA D0 05 04 00 00 1F 33 F8 C0 0A 	//CA D0 05 04 00 00 1F 46 C0 0A E4 	//CA D0 05 04 00 00 1F 66 A8 C0 00 	//CA D0 05 04 00 00 1F BA 59 C0 0A 	//CA D0 05 04 00 00 1F EF 09 C0 00 	//CA D0 05 04 00 00 20 81 C0 0A 58 	//CA D0 05 04 00 00 20 CF C0 00 F1 	//CA D0 05 04 00 00 21 02 10 C0 0A 	//CA D0 05 04 00 00 21 57 40 C0 00 	//CA D0 05 04 00 00 21 8B B1 C0 0A 	//CA D0 05 04 00 00 21 91 C0 0A 79 	//CA D0 05 04 00 00 21 DE E1 C0 00 	//CA D0 05 04 00 00 21 DF C0 00 D0 	//CA D0 05 04 00 00 22 A1 C0 0A 1A 	//CA D0 05 04 00 00 22 EF C0 00 B3 	//CA D0 05 04 00 00 23 37 84 C0 0A 	//CA D0 05 04 00 00 23 62 D4 C0 00 	//CA D0 05 04 00 00 23 B1 C0 0A 3B 	//CA D0 05 04 00 00 23 BE 25 C0 0A 	//CA D0 05 04 00 00 23 EB 75 C0 00 	//CA D0 05 04 00 00 23 FF C0 00 92 	//CA D0 05 04 00 00 24 2F 2A C0 00 	//CA D0 05 04 00 00 24 7A 7A C0 0A 	//CA D0 05 04 00 00 24 8F C0 00 75 	//CA D0 05 04 00 00 24 A6 8B C0 00 	//CA D0 05 04 00 00 24 C1 C0 0A DC 	//CA D0 05 04 00 00 24 F3 DB C0 0A 	//CA D0 05 04 00 00 25 9F C0 00 54 	//CA D0 05 04 00 00 25 D1 C0 0A FD 	//CA D0 05 04 00 00 26 1A BE C0 00 	//CA D0 05 04 00 00 26 4F EE C0 0A 	//CA D0 05 04 00 00 26 93 1F C0 00 	//CA D0 05 04 00 00 26 AF C0 00 37 	//CA D0 05 04 00 00 26 C6 4F C0 0A 	//CA D0 05 04 00 00 26 E1 C0 0A 9E */
+	parser_t dummy;
+	uint32_t i;
+	uint16_t dumpos;
+	uint8_t dataBuffer[100];
+
+	for (i = 0; i < 10000; i++){
+		dummy.packet.id = 0xD0;
+		dummy.packet.sub_id = 0x05;
+		dummy.packet.length = 0x04;
+		dummy.packet.payload[3] = i & 0xFF;
+		dummy.packet.payload[2] = (i >> 8) & 0xFF;
+		dummy.packet.payload[1] = (i>>16) & 0xFF;
+		dummy.packet.payload[0] = (i>>24) & 0xFF;
+
+		packet_eeprom_prepare_packet(&dummy, dataBuffer,&dumpos);
+		if (((dummy.crc & 0xFF) == 0xCA) | ((dummy.crc & 0xFF) == 0xC0)){
+			uart_OutBuffer(USARTx, dataBuffer, dumpos);
+			//break;
+		}else if (((dummy.crc & 0xFF00) == 0xCA00) | ((dummy.crc & 0xFF00) == 0xC000)){
+			uart_OutBuffer(USARTx, dataBuffer, dumpos);
+			//break;
+		}
+	}
+}
 
 void packet_eeprom_byte_stuff(uint8_t byte, uint8_t * buffer, uint16_t * index){
 	if (byte == STX || byte == ESC){
@@ -52,107 +85,196 @@ void packet_eeprom_byte_stuff(uint8_t byte, uint8_t * buffer, uint16_t * index){
 	}
 }
 
-void packet_eeprom_send_packet(USART_TypeDef * USARTx, uint8_t id, uint8_t * payload, uint8_t size){
-	uint16_t index = 0;
-	uint8_t sendBuffer[MAX_PAYLOAD_SIZE+20];
-	uint8_t i = 0;
-	uint16_t calculated_crc;
+packet_eeprom_t packet_eeprom_read_packet(USART_TypeDef * USARTx, uint32_t *address, parser_t * details){
+	//uint32_t data;
+	uint16_t i, j = 0;
+	bool keep_processing = true;
+	packet_eeprom_t packet_result;
+	//uint8_t dataBuffer[50];
+	union {
+		uint32_t u32;
+		uint8_t u8[4];
+	} data;
 
-	//  Need to Stuff bytes
-	sendBuffer[index++] = STX;
+	data.u32 = *(__IO uint32_t*)*address;
+	if (data.u8[0] != STX){
+		return 1;
+	}
 
-	packet_eeprom_byte_stuff(id, sendBuffer,&index);
-	packet_eeprom_byte_stuff(size, sendBuffer, &index);
+	//feed each byte into packet parser. finished when encounter STX or when
+	//packet is good.
 
-	for (i = 0; i < size; i++){
-		packet_eeprom_byte_stuff(payload[i], sendBuffer, &index);
+	while (keep_processing){
+		data.u32 = *(__IO uint32_t*)*address;
+		for (i = 0; i < 4; i++){
+			packet_result = packet_eeprom_parser(data.u8[i], details);
 
-		if (index > sizeof(sendBuffer)){
-			//Something is wrong here, return without sending
-			return;
+			if (packet_result == PKT_SUCCESS){
+				uint8_t dataBuffer[100];
+				uint16_t pos = 0;
+
+				packet_eeprom_prepare_packet(details,dataBuffer, &pos);
+				uart_OutBuffer(USARTx, dataBuffer, pos);
+				return PKT_SUCCESS;
+			}else if (packet_result == PKT_FAILURE){
+				return PKT_FAILURE;
+			}
+		}
+		*address = *address + 4;
+
+		j += 4;
+		if (j > MAX_PAYLOAD_SIZE){
+			return PKT_FAILURE;
 		}
 	}
-	// Calculate CRC on stuffed bytes
-	calculated_crc = crc_16bit_algorithm_dnp_calculate_full(sendBuffer, index, 0xFFFF, 0xFFFF);
-
-	// Stuff CRC if needed
-	packet_eeprom_byte_stuff((calculated_crc & 0xFF),sendBuffer, &index);
-	packet_eeprom_byte_stuff((calculated_crc >> 8), sendBuffer, &index);
-
-	uart_OutBuffer(USARTx, sendBuffer, index);
-
-	//Send
 }
 
-void packet_eeprom_parser(USART_TypeDef * USARTx, uint8_t byte){
-	static unsigned short crc = 0;
-	static bool byte_stuff = false;
-	static packet_t packet;
-	static uint8_t received_length;
+void packet_eeprom_write(uint8_t * buffer, uint16_t size, uint32_t *address){
+	uint32_t data;
+	uint16_t i, j;
+	__IO FLASH_Status FLASHStatus = FLASH_COMPLETE;
 
-	static packet_eeprom_t state = PKT_HEADER;
+	//enable flash writes here
+	DATA_EEPROM_Unlock();
+
+	FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
+				| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR | FLASH_FLAG_OPTVERRUSR);
+
+	//handle all 4 byte multiples
+	for (i = 0, j = 0; i < (size/4); i++){
+		data = (buffer[j+3] << 24) | (buffer[j+2] << 16) | (buffer[j+1] << 8) | buffer[j];
+
+		do{
+			FLASHStatus = DATA_EEPROM_ProgramWord(*address, data);
+
+			if(FLASHStatus == FLASH_COMPLETE){
+				*address = *address + 4;
+			}else{
+				FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
+						| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR);
+			}
+		}while (FLASHStatus != FLASH_COMPLETE);
+		j += 4;
+	}
+	//handle remainder
+	if (j < size){
+		data = buffer[j++];
+		if (j<size){
+			data = data | (buffer[j++] << 8);
+			if(j<size){
+				data = data | (buffer[j++] << 16);
+				if(j<size){
+					data = data | (buffer[j++] << 24);
+				}
+			}
+		}
+		do{
+			FLASHStatus = DATA_EEPROM_ProgramWord(*address, data);
+
+			if(FLASHStatus == FLASH_COMPLETE){
+				*address = *address + 4;
+			}else{
+				FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR
+						| FLASH_FLAG_SIZERR | FLASH_FLAG_OPTVERR);
+			}
+		}while (FLASHStatus != FLASH_COMPLETE);
+	}
+
+	DATA_EEPROM_Lock();
+}
+
+void packet_eeprom_prepare_packet(parser_t * details, uint8_t * sendBuffer, uint16_t * pos){
+	uint8_t i = 0;
+	*pos = 0;
+
+	//  Need to Stuff bytes
+	sendBuffer[*pos] = STX;
+	*pos = *pos +1;
+
+	packet_eeprom_byte_stuff(details->packet.id, sendBuffer, pos);
+	packet_eeprom_byte_stuff(details->packet.sub_id, sendBuffer, pos);
+	packet_eeprom_byte_stuff(details->packet.length, sendBuffer, pos);
+
+	for (i = 0; i < details->packet.length; i++){
+		packet_eeprom_byte_stuff(details->packet.payload[i], sendBuffer, pos);
+	}
+	// Calculate CRC on stuffed bytes
+	details->crc = crc_16bit_algorithm_dnp_calculate_full(sendBuffer, *pos, 0xFFFF, 0xFFFF);
+
+	// Stuff CRC if needed
+	packet_eeprom_byte_stuff((details->crc & 0xFF),sendBuffer, pos);
+	packet_eeprom_byte_stuff((details->crc >> 8), sendBuffer, pos);
+}
+
+void packet_parser_init(parser_t * details){
+	details->state = PKT_HEADER;
+	details->byte_stuff = false;
+}
+
+packet_eeprom_t packet_eeprom_parser(uint8_t byte, parser_t * details){
 
 	if (byte == STX){
-		crc = crc_16bit_algorithm_dnp_update(0xFFFF, byte);
-		state = PKT_ID;
-		return;
+		details->crc = crc_16bit_algorithm_dnp_update(0xFFFF, byte);
+		details->packet.stx = STX;
+		details->state = PKT_ID;
+		return details->state;
 	}
 
 	//Add byte to current crc
-	if (state > PKT_HEADER && state < PKT_CRC1){
-		crc = crc_16bit_algorithm_dnp_update(crc, byte);
+	if (details->state > PKT_HEADER && details->state < PKT_CRC1){
+		details->crc = crc_16bit_algorithm_dnp_update(details->crc, byte);
 	}
 
 	//Do byte stuffing
-	if(byte_stuff){
-		if (byte == ESC_NULL || byte == ESC_FIVE){
+	if(details->byte_stuff){
+		if (byte == ESC_NULL || byte == ESC_A){
 			byte = ESC | byte;
-			byte_stuff = false;
+			details->byte_stuff = false;
 		}else{
 			//Invalid case!
-			state = PKT_HEADER;
-			return;
+			details->state = PKT_HEADER;
+			details->byte_stuff = false;
+			return details->state;
 		}
 	}else if (byte == ESC){
-		byte_stuff = true;
-		return;
+		details->byte_stuff = true;
+		return details->state;
 	}
 
 	//Process state
-	if (state == PKT_ID){
-		packet.id = byte;
-		state = PKT_LENGTH;
-	}else if (state == PKT_LENGTH){
-		received_length = 0;
-		packet.length = byte;
-		if (packet.length == 0x00){
-			state = PKT_CRC1;
+	if (details->state == PKT_ID){
+		details->packet.id = byte;
+		details->state = PKT_SUB_ID;
+	}else if (details->state == PKT_SUB_ID){
+		details->packet.sub_id = byte;
+		details->state = PKT_LENGTH;
+	}else if (details->state == PKT_LENGTH){
+		details->received_length = 0;
+		details->packet.length = byte;
+		if (details->packet.length == 0x00){
+			details->state = PKT_CRC1;
 		}else{
-			state = PKT_PAYLOAD;
+			details->state = PKT_PAYLOAD;
 		}
-	}else if (state == PKT_PAYLOAD){
-		packet.payload[received_length++] = byte;
-		if (received_length == packet.length){
-			state = PKT_CRC1;
+	}else if (details->state == PKT_PAYLOAD){
+		details->packet.payload[details->received_length++] = byte;
+		if (details->received_length == details->packet.length){
+			details->state = PKT_CRC1;
 		}
-	}else if (state == PKT_CRC1){
-		packet.crc[0] = byte;
-		state = PKT_CRC2;
-	}else if (state == PKT_CRC2){
-		uint16_t received_crc;
-		packet.crc[1] = byte;
-		received_crc = (packet.crc[1] << 8) | packet.crc[0];
-		crc = crc ^ 0xFFFF;
+	}else if (details->state == PKT_CRC1){
+		details->packet.crc[0] = byte;
+		details->state = PKT_CRC2;
+	}else if (details->state == PKT_CRC2){
+		details->packet.crc[1] = byte;
+		details->crc = details->crc ^ 0xFFFF;
 
-		if (crc == received_crc){
-			//crc matches.
-			uart_OutString(USARTx,"CRC Matches!\r\n");
-			packet_eeprom_send_packet(USARTx, packet.id, packet.payload,packet.length);
+		if (details->crc == ((details->packet.crc[1] << 8) | details->packet.crc[0])){
+			details->state = PKT_SUCCESS;
 		}else{
-			char buffer[40];
-			sprintf(buffer,"CRC Fail, Calc %04x, Recv %04x\r\n",crc, received_crc);
-			uart_OutString(USARTx, buffer);
+			details->state = PKT_FAILURE;
 		}
-		state = PKT_HEADER;
+	}else{
+		details->state = PKT_HEADER;
 	}
+	return details->state;
 }
